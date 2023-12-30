bef967d5f9c719df8fc26d0c87cb14d0f42d169f
netoe1 <elynetobr@outlook.com>
2023-12-30 04:10:35 -0300

    updating with script

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,8 +2,8 @@
 #define UTILS_HPP
 #define OPTION_NODE "nodejs"
 #define OPTION_C "c"
-#define OPTION_CPLUS "c++"
+#define OPTION_CPLUS_1 "c++"
+#define OPTION_CPLUS_2 "cpp"
 #define PNAME_PREFIX_ERR "[aefi-err]:"
 #define PNAME_PREFIX_FINE "[aefi-fine]:"
-
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,9 @@ string flag;
 
 // <program> <function> <language>
 
+void setupVariables();
+void help(); // Help function for CLI.
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "");
@@ -26,11 +29,20 @@ int main(int argc, char *argv[])
     {
         if (argc != 3)
         {
-                }
+            throw "You set an invalid type of parameters."
+        }
     }
     catch (string &msg)
     {
         cout << PNAME_PREFIX_ERR << msg << endl;
     }
     return 0;
+}
+
+void help()
+{
+    cout << endl;
+    cout << "[aefi-help]: Valid functions :" << endl;
+    cout << "\t\t--help || -h \t-- Show valid commands to use" << endl;
+    cout << "\t create <lang> \t Create an project in the same program's directory" << endl;
 }
